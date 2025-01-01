d1e8265d658cdd5884a7e04db328442e6a42db5a
May <t0333024@my.ntu.ac.uk>
2025-01-01 22:09:22 +0000

    Modify Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,2 +1,19 @@
-const Home = () => <h1>Welcome to UK Dorm</h1>;
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/Home.css";
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>Welcome to UK Dorm</h1>
+            <p>Your gateway to find the perfect accommodation in the UK.</p>
+            <div className="home-buttons">
+                <Link to="/rooms" className="home-button">Browse Room</Link>
+                <Link to="/login" className="home-button">Login</Link>
+                <Link to="/register" className="home-button">Register</Link>
+            </div>
+        </div>
+    )
+};
+
 export default Home;

aee306538a5192fc4e82f7a406320e036bb63208
May <t0333024@my.ntu.ac.uk>
2025-01-05 23:33:35 +0000

    Changed the api baseURl to only Cloud backend url

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:8080" || process.env.REACT_APP_BACKEND_URL,
+    // baseURL: "http://localhost:8080",
+    baseURL: process.env.REACT_APP_BACKEND_URL,
     headers: {
         'Content-Type': 'application/json',
     },
